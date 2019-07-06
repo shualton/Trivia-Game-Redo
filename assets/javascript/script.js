732d2a42548abd79ebc1bd0bfba66a103d3769c4
shualton <shualton@gmail.com>
2019-07-06 16:26:39 -0700

    added some questions to script

---
+++ assets/javascript/script.js
@@ -0,0 +1,36 @@
+var questions = [{
+    ques: "What year was the NBA founded?",
+    ans: ["2019", "1967", "1946", "1932"],
+    name: "firstYear",
+    correct: "1946",
+    divClass: ".firstYear"
+},
+{
+    ques: "How many teams are in the NBA?",
+    ans: ["32", "30", "28", "26"],
+    name: "teams",
+    correct: "30",
+    divClass: ".teams"
+},
+{
+    ques: "What is the name of the NBA Championship Trophy?",
+    ans: ["Heisman Trophy", "Stanley Trophy", "Vince Lombardi Trophy", "Larry O'brien Trophy"],
+    name: "trophy",
+    correct: "Larry O'brien Trophy",
+    divClass: ".trophy"
+},
+{
+    ques: "Which player holds the record for most points scored in a single game?",
+    ans: ["Wilt Chaimberlain", "Bill Russell", "Michael Jordan", "Kareem Abdul-Jabar"],
+    name: "points",
+    correct: "Wilt Chaimberlain",
+    divClass: ".points"
+},
+{
+    ques: "Who is the only NBA player to win Finals MVP while on the losing team?",
+    ans: ["Lebron James", "Michael Jordan", "Jerry West", "Magic Johnson"],
+    name: "fmvp",
+    correct: "Jerry West",
+    divClass: ".fmvp"
+},
+] 
